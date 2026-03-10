63b313952ea096476008e090c13a412288944ad3
moaz-shamim <moaz.shamim1575@gmail.com>
2026-03-10 21:09:57 +0530

    Implement react icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FaUserCircle } from "react-icons/fa";
+
 export default function Home() {
   const [posts, setPosts] = useState([]);
   console.log("posts",posts);
@@ -107,18 +109,7 @@ export default function Home() {
               <p class="overflow-hidden max-h-36">{parse(firstPost.content)}</p>
 
               <div className="flex space-x-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-5 h-5 dark:text-gray-400"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
+                <FaUserCircle className="w-5 h-5 dark:text-gray-400" />
                 <span className="self-center text-sm">
                   by {firstPost.userName}
                 </span>
